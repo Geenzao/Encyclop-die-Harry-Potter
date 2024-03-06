b0986401d76cf9ef7d13ce9e3ed2e052e4c32dda
Geenzao <quentinlorenzo04@gmail.com>
2024-03-06 12:07:09 +0100

    Ajout de composant et ajout du sommaire

---
+++ encyclopedie-harry-potter/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+  import SommaireComp from './components/Sommaire/SommaireComp.vue'
+
+  export default {
+    components: {
+      SommaireComp
+    }
+  }
+</script>
+
+<template>
+    <SommaireComp/>
+
+    <router-view></router-view>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>./components/Sommaire/SommaireComp.vue
