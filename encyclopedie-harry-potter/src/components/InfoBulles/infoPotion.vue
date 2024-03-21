1758e1d1d7ee67613bc0e328230cfaf04cc18623
Geenzao <quentinlorenzo04@gmail.com>
2024-03-21 15:33:17 +0100

    L'url de la page est bonne

--- encyclopedie-harry-potter/src/components/InfoBulles/infoPotion.vue
+++ encyclopedie-harry-potter/src/components/InfoBulles/infoPotion.vue
@@ -11,16 +11,38 @@ export default {
 
 <template>
     <!-- La div qui représente l'infobulle de la potion -->
-    <a :href="potion.lienPotion"><div>
-        <!-- Image de la potion-->
-        <div>
-            <img :src= "potion.imagePotion" alt="Image représentant la potion">
+    <router-link :to="'/potions/' + potion.nomPotion" class="router-link">
+        <div class="popup">
+            <!-- Image de la potion--> 
+            <div>
+                <img :src= "potion.imagePotion" alt="Image représentant la potion">
+            </div>
+            <div>
+                <p>{{potion.nomPotion}} </p>
+            </div>
         </div>
-        <div>
-            <p>{{potion.nomPotion}} </p>
-        </div>
-    </div></a>
+    </router-link>
 </template>
 
 <style scoped>
+    .popup {
+        max-width: 300px;
+        max-height: 500px;
+        background-color: white;
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+    .popup img {
+        max-width: 100%;
+        height: auto;
+        margin-bottom: 10px;
+    }
+    .popup p {
+        margin: 0;
+    }
+    .router-link {
+        text-decoration: none;
+        color: inherit; /* Utilisez la couleur par défaut */
+    }
 </style>
---
+++ encyclopedie-harry-potter/src/components/Pages/onePotionPage.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <h1>{{ $route.params.id }}</h1>
+    </div>
+</template>
+
+<style scoped>
+</style>
